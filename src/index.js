1e12985877441d31a6a0a5956d07608db9791671
SouravR7 <routhsouravrouth2015@outlook.com>
2020-12-16 03:05:38 +0530

    changii

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 
-const onePageArticleCount = 10
-
+const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const { newsArticleModel } = require("./connector");
 
+app.get("/newFeeds", (req, res) => {
+  let limitReceived = req.query.limit;
+  let offsetReceived = req.query.offset;
+  let limit = parseInt(Number(limitReceived));
+  let offset = parseInt(Number(offsetReceived));
+  if (!limitReceived) {
+    limit = onePageArticleCount;
+  }
+  if (!offsetReceived) {
+    offset = 0;
+  }
+  if (isNaN(offset)) {
+    offset = 0;
+  }
+  if (isNaN(limit) || limit < 0) {
+    limit = onePageArticleCount;
+    if (!offsetReceived) {
+      offset = 0;
+    }
+  }
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+  const options = {};
+  options.skip = offset;
+  options.limit = limit;
+  newsArticleModel.countDocuments({}, function (err) {
+    if (err) {
+      res.status(500).send({ message: err.message });
+      return;
+    }
+    newsArticleModel.find({}, {}, options, function (err, result) {
+      if (err) {
+        res.status(500).send({ message: err.message });
+        return;
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
+});
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
